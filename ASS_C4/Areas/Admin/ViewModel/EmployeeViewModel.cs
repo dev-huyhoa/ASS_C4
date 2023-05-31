9db385039dcf4b57bbc0b37fc932c54f004c7e9e
DESKTOP-I0VVPLJ\HIS <driverhuyhoa@gmail.com>
2023-05-31 19:08:47 +0700

    tạo viewmodel

---
+++ ASS_C4/Areas/Admin/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ASS_C4.Areas.Admin.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public Guid IdEmployee { get; set; }
+        public string NameEmployee { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public long Birthday { get; set; }
+        public string UrlImage { get; set; }
+        public string Password { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsActice { get; set; }
+        public bool IsDelete { get; set; }
+        public int RoleId { get; set; }
+        public string NameRole { get; set; }
+    }
+}
